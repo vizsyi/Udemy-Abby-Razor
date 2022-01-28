3a20824b4e46f58654c4e7d46b01d801b2ec0ade
steve.visy@gmail.com <steve.visy@gmail.com>
2022-01-28 19:52:43 +0100

    N-Tier Architecture (supplement)

--- AbbyWeb/Program.cs
+++ AbbyWeb/Program.cs
@@ -1,4 +1,6 @@
 using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository;
+using Abby.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfwork>();
 
 var app = builder.Build();
 
